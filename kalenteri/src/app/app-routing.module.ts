023fdcab39661f383420648268bd98684f1d1cf0
Henri Parviainen <henri.parviainen@icloud.com>
2020-01-16 20:55:24 +0200

    Viikko muutettu alkamaan maanantaista, lisätty bootstrap

--- kalenteri/src/app/app-routing.module.ts
+++ kalenteri/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { KalenteriComponent } from "./kalenteri/kalenteri.component";
+import { AktiivisetKurssitComponent } from "./aktiiviset-kurssit/aktiiviset-kurssit.component";
+import { SuoritetutKurssitComponent } from "./suoritetut-kurssit/suoritetut-kurssit.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "kalenteri", component: KalenteriComponent },
+  { path: "aktiiviset", component: AktiivisetKurssitComponent },
+  { path: "suoritetut", component: SuoritetutKurssitComponent },
+  { path: "", component: KalenteriComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
