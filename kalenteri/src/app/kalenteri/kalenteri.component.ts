eae1c0248704049a8625de0bbadd2e2a264d7eba
Henri Parviainen <henri.parviainen@icloud.com>
2020-01-16 13:50:21 +0200

    muutettu sovelluksen rakennetta

--- kalenteri/src/app/kalenteri/kalenteri.component.ts
+++ kalenteri/src/app/kalenteri/kalenteri.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import {
+  EventSettingsModel,
+  DayService,
+  WeekService,
+  WorkWeekService,
+  AgendaService
+} from "@syncfusion/ej2-angular-schedule";
 
 @Component({
-  selector: 'app-kalenteri',
-  templateUrl: './kalenteri.component.html',
-  styleUrls: ['./kalenteri.component.css']
+  selector: "app-kalenteri",
+  templateUrl: "./kalenteri.component.html",
+  styleUrls: ["./kalenteri.component.css"],
+  providers: [WeekService, AgendaService]
 })
-export class KalenteriComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class KalenteriComponent {
+  public selectedDate: Date = new Date(2018, 1, 15);
+  public views: Array<string> = ["Day", "Week", "WorkWeek"];
+  public showHeaderBar: Boolean = false;
+  title = "kalenteri";
 }
