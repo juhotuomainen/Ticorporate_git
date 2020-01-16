84840aed9a380b8d3bcc68b13273bca17e77de51
Henri Parviainen <henri.parviainen@icloud.com>
2020-01-16 13:11:01 +0200

    demo lisätty

---
+++ kalenteri/src/app/kalenteri/kalenteri.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KalenteriComponent } from './kalenteri.component';
+
+describe('KalenteriComponent', () => {
+  let component: KalenteriComponent;
+  let fixture: ComponentFixture<KalenteriComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ KalenteriComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KalenteriComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ kalenteri/src/app/kalenteri/kalenteri.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kalenteri',
+  templateUrl: './kalenteri.component.html',
+  styleUrls: ['./kalenteri.component.css']
+})
+export class KalenteriComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
