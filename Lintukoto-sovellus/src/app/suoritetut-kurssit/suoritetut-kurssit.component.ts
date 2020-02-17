e8652c9694f6839e58a47f3defe56ce372df166a
Rasmus Savolainen <rasmussavolainen@hotmail.com>
2020-02-17 12:24:37 +0200

    tehty suoritetutkurssit sivua eteenpäin. Ajakaa npm i

--- Lintukoto-sovellus/src/app/suoritetut-kurssit/suoritetut-kurssit.component.ts
+++ Lintukoto-sovellus/src/app/suoritetut-kurssit/suoritetut-kurssit.component.ts
@@ -1,37 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from "@angular/core";
+import Swiper from "swiper";
 
 @Component({
-  selector: 'app-suoritetut-kurssit',
-  templateUrl: './suoritetut-kurssit.component.html',
-  styleUrls: ['./suoritetut-kurssit.component.css']
+  selector: "app-suoritetut-kurssit",
+  templateUrl: "./suoritetut-kurssit.component.html",
+  styleUrls: ["./swiper.min.css", "./suoritetut-kurssit.component.css"]
 })
-export class SuoritetutKurssitComponent implements OnInit {
-  title: 'Suoritetut kurssit';
+export class SuoritetutKurssitComponent implements AfterViewInit {
+  title: "Suoritetut kurssit";
+  mySwiper: Swiper;
   // tslint:disable-next-line: ban-types
   kurssit: Array<Object> = [
     {
-      Kurssikoodi: 'X301',
-      nimi: 'Web sovelluskehitys',
-      kuva: '../../assets/images/orsipollo4.png'
+      Kurssikoodi: "X301",
+      nimi: "Web sovelluskehitys",
+      kuva: "../../assets/images/pinkkihyrrahattu.png"
     },
     {
-      Kurssikoodi: 'X302',
-      nimi: 'Tietokannat',
-      kuva: '../../assets/images/home.png'
+      Kurssikoodi: "X302",
+      nimi: "Tietokannat",
+      kuva: "../../assets/images/pinkkihyrrahattu.png"
     },
     {
-      Kurssikoodi: 'X303',
-      nimi: 'Yrittäjyys',
-      kuva: '../../assets/images/pollomalli.png'
+      Kurssikoodi: "X303",
+      nimi: "Yrittäjyys",
+      kuva: "../../assets/images/pollomalli.png"
     },
     {
-      Kurssikoodi: 'X304',
-      nimi: 'Web-liiketoiminta',
-      kuva: '../../assets/images/pollomalli3.0_surullinen.png'
+      Kurssikoodi: "X304",
+      nimi: "Web-liiketoiminta",
+      kuva: "../../assets/images/pollomalli3.0_surullinen.png"
     }
   ];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper(".swiper-container", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets"
+      }
+    });
+  }
 }
