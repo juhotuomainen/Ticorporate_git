06a6a83cd9eefd9fd6bf025a39b71cffed5f13f0
Rasmus Savolainen <rasmussavolainen@hotmail.com>
2020-02-21 09:46:11 +0200

    iconi lisätty

---
+++ Lintukoto-sovellus/src/app/henkipollo/henkipollo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-henkipollo",
+  templateUrl: "./henkipollo.component.html",
+  styleUrls: ["./henkipollo.component.css"]
+})
+export class HenkipolloComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
