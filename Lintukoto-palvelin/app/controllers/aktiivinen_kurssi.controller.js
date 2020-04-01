8e38956f599cce068fdef23c97f176a66af2adc4
Henri Parviainen <henri.parviainen@icloud.com>
2020-04-01 12:21:46 +0300

    kommentoitu ja siistitty koodia, poistettu turhaa koodia

--- Lintukoto-palvelin/app/controllers/aktiivinen_kurssi.controller.js
+++ Lintukoto-palvelin/app/controllers/aktiivinen_kurssi.controller.js
@@ -6,16 +6,23 @@ const url = require('../../config/database.config');
 
 const mongoose = require('mongoose');
 
+// UUDEN KURSSIN LISÄYS
+
 exports.create = (req, res) => {
-  // Validate request
+  // Tarkistetaan, että requestin mukana tulee body, ja jos ei tule lähetetään virheilmoitus.
   if (!req.body) {
     return res.status(400).send({
-      message: 'Note content can not be empty'
+      message: 'Virhe! Kurssia ei löytynyt.'
     });
   }
-  console.log(req.body.nimi);
+
   let opintopisteet;
   let tehtavat;
+
+  // Etsitään tietokannan 'kurssi' collectionista match, jonka nimi matchaa req.body.nimeen.
+  // -> Tallennetaan opintopisteet ja tehtavat ko. muuttujiin
+  // -> Luodaan "aktiivinen" Objecti, josta tehdään uusi aktiivinen kurssi.
+  // - >Lopuksi pushataan luotu "aktiivinen" Objekti oikean käyttäjän tietoihin.
 
   Kurssi.findOne({ nimi: req.body.nimi })
     .then(kurssi => {
@@ -47,89 +54,17 @@ exports.create = (req, res) => {
         { upsert: true }
       ).then(res.status(200).redirect('http://localhost:4200/aktiiviset'));
     });
-
-  // const aktiivinen = new AktiivinenKurssi({
-  //   kurssikoodi: req.body.kurssikoodi,
-  //   nimi: req.body.nimi,
-  //   kuva: req.body.kuva,
-  //   muistiinpanot: req.body.muistiinpanot
-  // });
-  // const useri = Kayttaja.findOne({ tunnus: req.body.tunnus });
-  // console.log(useri.nimi + 'Testaushommia 666 !!!!!!!!!!');
-  // useri.kurssit.update(aktiivinen);
-
-  // useri
-  //   .save()
-  //   .then(result => {
-  //     res.status(200).redirect('http://localhost:4200/aktiiviset');
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send({
-  //       message: err.message || 'Some error occurred while creating the Note.'
-  //     });
-  //   });
 };
+
+// ETSITÄÄN TIETTY KÄYTTÄJÄ JA PALAUTETAAN SEN TIEDOT
 
 exports.findAll = (req, res) => {
-  console.log('tunnus --->>' + req.query.tunnus);
   Kayttaja.Kayttaja.findOne({ tunnus: req.query.tunnus }).then(result => {
-    console.log(result);
     res.send(result);
   });
-  // AktiivinenKurssi.find()
-  //   .then(notes => {
-  //     res.send(notes);
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send({
-  //       message: err.message || 'Some error occurred while retrieving notes.'
-  //     });
-  //   });
 };
 
-exports.find = (req, res) => {
-  console.log(req.body);
-  AktiivinenKurssi.find(req.body.kurssikoodi)
-    .then(note => {
-      if (!note) {
-        return res.status(404).send({
-          message: 'Kurssia ei löytynyt' + req.body.kurssikoodi
-        });
-      }
-      res.send(note);
-    })
-    .catch(err => {
-      if (err.kind === 'ObjectId') {
-        return res.status(404).send({
-          message: 'Note not found with id ' + req.body.kurssikoodi
-        });
-      }
-      return res.status(500).send({
-        message: 'Error retrieving note with id ' + req.body.kurssikoodi
-      });
-    });
-};
-
-exports.finderi = (req, res, next) => {
-  const kurssikoodi1 = req.body.kurssi;
-  const muistiinpanoObject = {
-    kurssi: req.body.kurssi,
-    muistiinpano: req.body.muistiinpano,
-    otsikko: req.body.otsikko
-  };
-
-  AktiivinenKurssi.find({ kurssikoodi: kurssikoodi1 }, (err, mp) => {
-    console.log('lol');
-    mp.muistiinpanot.$push(muistiinpanoObject);
-    mp.save();
-  })
-    .then(res.send('ok'))
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || 'Some error occurred while creating the Note.'
-      });
-    });
-};
+// ETSITÄÄN KÄYTTÄJÄ JA LISÄTÄÄN TÄLLE UUSI MUISTIINPANO
 
 exports.findjaupdate = (req, res) => {
   if (!req.body) {
@@ -139,7 +74,6 @@ exports.findjaupdate = (req, res) => {
   }
   console.log(req.body);
 
-  // const user =  Kayttaja.Kayttaja.findOne({ tunnus: req.body.tunnus });
   Kayttaja.Kayttaja.updateOne(
     {
       tunnus: req.body.tunnus,
@@ -151,33 +85,15 @@ exports.findjaupdate = (req, res) => {
     .catch(err => {
       console.log(err);
     });
-
-  // const muistiinpano = new Muistiinpano({
-  //   kurssi: req.body.kurssi,
-  //   otsikko: req.body.otsikko,
-  //   muistiinpano: req.body.muistiinpano,
-  //   tunnus: req.body.tunnus
-  // });
-  // muistiinpano.save().then(result => {
-  //   const user = Kayttaja.Kayttaja.findOne({ tunnus: req.body.tunnus });
-  //   for (i in user.aktiiviset_kurssit.nimi) {
-  //     if (user.aktiiviset_kurssit.nimi[i] == req.body.kurssi) {
-  //       user.aktiiviset_kurssit.muistiinpanot.push(result);
-  //       user.save();
-  //     }
-  //   }
-  //   res
-  //     .status(200)
-  //     .json({ message: 'muistiinpano lisätty' })
-  //     .redirect('http://localhost:4200/aktiiviset');
-  // });
 };
 
+// LUODAAN UUSI AKTIIVNEN KURSSI KÄYTTÄJÄLLE
+
 exports.createKurssi = (req, res) => {
-  // Validate request
+  // Validoidaan pyyntö
   if (!req.body) {
     return res.status(400).send({
-      message: 'Note content can not be empty'
+      message: 'Valitse kurssi pudotusvalikosta. Kurssi ei voi olla tyhjä!'
     });
   }
 
@@ -198,6 +114,8 @@ exports.createKurssi = (req, res) => {
       });
     });
 };
+
+// ETSITÄÄN KAIKKI KURSSIT
 
 exports.findAllKurssi = (req, res) => {
   Kurssi.find()
